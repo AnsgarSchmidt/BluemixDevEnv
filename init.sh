f2ce86267e261cb5a3431076f8efe28edb44171b
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-07-06 20:51:18 +0200

    Changing environment variable include

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 cf login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.ng.bluemix.net -o ${BX_ORG} -s ${BX_SPACE}
 
 bx login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.ng.bluemix.net -o ${BX_ORG} -s ${BX_SPACE} -c ${BX_ACCOUNT_ID}
-bx ic init | grep export >> /root/.bashrc
+bx ic init | grep export >> /root/.bx_dockerrc
 
 wsk property set --apihost openwhisk.ng.bluemix.net --auth ${WSK_AUTH}
 wsk sdk install docker
 
-. .bashrc
+. .bx_dockerrc 
 docker ps
 
 echo "Please execute these lines:"
-cat .bashrc
+echo ". .bx_dockerrc"
