b099fc54a90b43f84045bcf25079bcc23a4e856b
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-12-07 18:11:43 +0100

    Remove docker native support

--- init.sh
+++ init.sh
@@ -1,26 +1,12 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 cf login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.ng.bluemix.net -o ${BX_ORG} -s ${BX_SPACE}
 
 bx login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.ng.bluemix.net -o ${BX_ORG} -s ${BX_SPACE} -c ${BX_ACCOUNT_ID}
-bx ic init | grep export >> /root/.bx_dockerrc
-bx cs init
 
 wsk property set --apihost openwhisk.ng.bluemix.net --auth ${WSK_AUTH}
 wsk sdk install docker
 wsk sdk install bashauto
 
-. .bx_dockerrc 
-docker ps
-
+bx cs init
 bx cs cluster-config ${KUB_CLUSTER}
-
-echo "----------------------------------"
-echo "-- Please execute these lines   --"
-echo "-- In order to set environment  --"
-echo "-- variables for the docker     --"
-echo "-- client and wsk autocomplete: --"
-echo "----------------------------------"
-echo ". .bx_dockerrc"
-echo ". wsk_cli_bash_completion.sh"
-echo ""
