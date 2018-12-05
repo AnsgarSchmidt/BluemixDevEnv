d9de83a5a0e57448ded1c8e0fd38bd23da969cf5
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2018-12-05 13:07:24 +0100

    switching to eu

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cf login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.ng.bluemix.net -o ${BX_ORG} -s ${BX_SPACE}
+cf login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.eu-gb.bluemix.net -o ${BX_ORG} -s ${BX_SPACE}
 
-bx login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.ng.bluemix.net -o ${BX_ORG} -s ${BX_SPACE} -c ${BX_ACCOUNT_ID}
+bx login -u ${BX_USERNAME} -p ${BX_PASSWORD} -a api.eu-gb.bluemix.net -o ${BX_ORG} -s ${BX_SPACE} -c ${BX_ACCOUNT_ID}
 
-wsk property set --apihost openwhisk.ng.bluemix.net --auth ${WSK_AUTH}
+wsk property set --apihost openwhisk.eu-gb.bluemix.net --auth ${WSK_AUTH}
 wsk sdk install docker
 wsk sdk install bashauto
 
